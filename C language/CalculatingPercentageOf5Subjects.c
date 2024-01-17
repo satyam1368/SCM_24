4be266ae7cad9d8798a310bce1266c2a69fda861
satyam1368 <156879225+satyam1368@users.noreply.github.com>
2024-01-17 11:12:27 +0530

    Add files via upload

---
+++ C language/CalculatingPercentageOf5Subjects.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*int main(){
+    float m1 = 32; //maths marks
+    float m2 = 38; //physics marks
+    float m3 = 28; //chem marks
+    float m4 = 35; //english marks
+    float p = (m1+m2+m3+m4)/4;
+    printf("percentage is %f",p);
+    return 0;
+}*/
+int main(){
+    float radius = 5;
+    float pi = 3.14;
+    float area = pi*radius*radius;
+    printf("area of circle is %f", area);
+    
+
+}
---
+++ C language/first.c
@@ -0,0 +1,44 @@
+#include<stdio.h> /*std- standar ,i-input , o - output , .h -header file  */
+
+// int main()
+// {
+//    printf("raghav garg ");
+//    return 0;
+// }
+
+//variables
+// int main(){
+//    int x;       // %d is used for int function.
+//    x=7;
+//    printf("%d",x);    
+//    x=x+5;
+//    printf("\n ");
+//    printf("%d",x);
+//    x=x-20;
+//    printf("\n ");
+//    printf("%d",x);
+
+//    return 0;
+// }
+// int main(){
+//    int x = 5;
+//    int y = 2;
+//    printf("%d\n",x+y);
+//    printf("%d\n",x-y);
+//    printf("%d\n",x/y);
+//    printf("%d\n",x*y);
+
+//    return 0;
+// }
+// int main(){
+//    int x = 4;
+//    int y = 2;
+//    int z = x-y;
+//    printf("%d",z);
+//    return 0;
+// }
+int main(){
+   printf("hello world\n ");
+   printf("hello world");
+   return 0;
+}
